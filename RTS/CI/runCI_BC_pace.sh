81ad31169840de594a48eaa1e27685df288d48e0
Janice Kim <janice.kim@noaa.gov>
2025-05-27 09:43:59 -0400

    Adding some test_case = 12 scripts

--- RTS/CI/runCI_BC_pace.sh
+++ RTS/CI/runCI_BC_pace.sh
@@ -12,10 +12,18 @@ export SCRATCHDIR="/home/${USER}/SHiELD_dev/SCRATCH/soloCI_amdbox_${RELEASE}/"
 
 mkdir -p ${BUILDDIR}/RTS/CI/stdout
 
+timestamp=$(date "+%Y%m%d_%H%M%S")
+echo "Script started at: $timestamp"
+
 SBATCHARGS="--account=${ACCOUNT} --time=00:60:00 --clusters=c5 --output=./stdout/%x.o%j --mail-user=${USER}@noaa.gov --mail-type=fail"
-./C96.solo.BCmoist.pace_64
-./C96.solo.BCmoist.pace_32
+# Test Case 13
+./C96.solo.BCmoist.pace_64 2>&1 | tee log.C96.solo.BCmoist.pace_64.${timestamp}.txt
+./C96.solo.BCmoist.pace_32 2>&1 | tee log.C96.solo.BCmoist.pace_32.${timestamp}.txt
+
+# Test Case 12
+./C96.solo.BCmoist.pace_12_64 2>&1 | tee log.C96.solo.BCmoist.pace_12_64.${timestamp}.txt
+./C96.solo.BCmoist.pace_12_32 2>&1 | tee log.C96.solo.BCmoist.pace_12_32.${timestamp}.txt
 
 
 # Existing BC moist, non-hydrostatic, sanity check
-#./C96.solo.BCmoist
+./C96.solo.BCmoist 2>&1 | tee log.C96.solo.BCmoist.${timestamp}.txt
