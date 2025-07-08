6950ff5f1c068ae825a65e1b81a7704f6c30424b
Janice Kim <janice.kim@noaa.gov>
2025-07-08 10:46:49 -0400

    Initial 3d modon soliton runs

---
+++ RTS/CI/runCI_3D_ModonSoliton.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This script will run all CI tests on c5
+# Modify ACCOUNT to be your group
+# Modify BUILDDIR to be where you have cloned the SHiELD_build repository
+
+export COMPILER="intel"
+#export COMP="repro"
+ACCOUNT="gfdl_f"
+export BUILDDIR="/home/${USER}/SHiELD_dev/SHiELD_build"
+RELEASE="`cat ${BUILDDIR}/../SHiELD_SRC/release`"
+export SCRATCHDIR="/home/${USER}/SHiELD_dev/SCRATCH/soloCI_amdbox_${RELEASE}/"
+
+mkdir -p ${BUILDDIR}/RTS/CI/stdout
+
+timestamp=$(date "+%Y%m%d_%H%M%S")
+echo "Script started at: $timestamp"
+
+SBATCHARGS="--account=${ACCOUNT} --time=00:60:00 --clusters=c5 --output=./stdout/%x.o%j --mail-user=${USER}@noaa.gov --mail-type=fail"
+
+test_name=C128.solo.3dmodon_64_debug
+./${test_name} | tee log.${test_name}.${timestamp}.txt
+
